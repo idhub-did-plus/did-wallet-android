f7f1a7fa4e3bab177bf797f35245b5f1d937a5e3
lyw <liyuwei2010@foxmail.com>
2020-03-18 17:55:51 +0800

    collection

--- app/src/main/java/com/idhub/wallet/net/collectiables/CollectionContent.java
+++ app/src/main/java/com/idhub/wallet/net/collectiables/CollectionContent.java
@@ -4,10 +4,9 @@ public class CollectionContent {
 
     //https://docs.opensea.io/reference#api-overview  opensea网站api
 
-//    public static String BASE_URL = "https://api.opensea.io/api/";
+    //    public static String BASE_URL = "https://rinkeby-api.opensea.io/";
+    public static String BASE_URL = "https://api.opensea.io/";
     public static String OPEN_SEA_LINK = "https://opensea.io/assets";
-    public static String BASE_URL = "https://rinkeby-api.opensea.io/";
-
     public static final String COLLECTIONS = "/api/v1/collections";
     public static final String ASSETS = "/api/v1/assets";
 
